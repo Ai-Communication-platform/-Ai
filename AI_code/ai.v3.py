8d9513426e91fd58f0c0315ca74b9393cd059a05
Giheung Kim <ewqdsacxzzaqxswcde@gmail.com>
2023-11-13 04:35:58 +0900

    2023-11-13
    gpt-3.5-turbo&ko input의 completion이 느림

--- AI_code/ai.v3.py
+++ AI_code/ai.v3.py
@@ -28,7 +28,7 @@ import pygame
 last_checked_time = time.time()
 
 # ChatGPT API Key Load
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 """---
@@ -98,7 +98,7 @@ def job():
                 #시작
                 start = time.time()
                 # 모델 - GPT 3.5 Turbo 선택
-                model = "gpt-3.5-turbo-1106"
+                model = "gpt-3.5-turbo"
                 # 질문 작성하기
                 #query = "다음 문서를 요약해줘: " + Message_text
                 query =f"""
@@ -111,7 +111,7 @@ def job():
                 승인 (Affirmation): 상대방의 진정한 가치를 인정해주는 것. 예: "너는 소중한 존재다."
                 힐링의 중요성: 사람들이 위로를 받고 싶어하는 이유는 마음이 따뜻해지면 사람이 움직이게 되고, 치유하는 방향으로 나아가게 되기 때문이다.
 
-                상기 글을 참고하세요. 그 후, 다음 글을 대학친구의 입장이 되어서 간단 명료하게 답하시오.
+                상기 글을 참고하세요. 그 후, 다음 글을 같은 나이인 친구의 입장이 되어서 간단 명료하게 답하시오.
                 "{Message_text}"
                 """
                 # 메시지 설정하기
@@ -158,42 +158,35 @@ def job():
                 ---
                 """
 
-                #시작
+                #TTS시작
                 start = time.time()
                 # Google Cloud TTS 인증 키 파일 경로 (서비스 계정 키)
                 tts_credentials_path = "C:\\Users\\win\\Documents\\GitHub\\-Ai\\ai-i-401313-176ecd5ad2cf.json"
-
                 # TTS 클라이언트 초기화
                 tts_client = texttospeech.TextToSpeechClient.from_service_account_json(tts_credentials_path)
-
-
                 def synthesize_text_to_audio(text, output_filename="C:\\Users\\win\\Documents\\GitHub\\-Ai\\tts_output.mp3"):
                     # 텍스트 설정
                     synthesis_input = texttospeech.SynthesisInput(text=text)
-
                     # 음성 설정
                     voice = texttospeech.VoiceSelectionParams(
                         name="ko-KR-Standard-D",
                         language_code="ko-KR",
                         ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
                     )
-
                     # 오디오 설정
                     audio_config = texttospeech.AudioConfig(
                         audio_encoding=texttospeech.AudioEncoding.MP3
                     )
-
                     # TTS 요청 및 응답
                     response = tts_client.synthesize_speech(
                         input=synthesis_input, voice=voice, audio_config=audio_config
                     )
-
                     # 오디오 파일로 저장
                     with open(output_filename, "wb") as out:
                         out.write(response.audio_content)
                         print(f"Audio content written to '{output_filename}'")
                 
-                # 현재 시간을 얻습니다.
+                # 현재 시간
                 current_time = datetime.now()
                 # "년월일시분" 형식으로 포맷팅합니다.
                 formatted_time = current_time.strftime("%Y%m%d%H%M%S")
