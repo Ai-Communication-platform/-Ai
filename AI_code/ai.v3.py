d6a974cd691e4ae45dd7f64517df232fd64904e9
yewon-yw <kimyewon0401@gmail.com>
2023-11-17 23:50:47 +0900

    Update ai.v3.py

--- AI_code/ai.v3.py
+++ AI_code/ai.v3.py
@@ -28,11 +28,7 @@ import pygame
 last_checked_time = time.time()
 
 # ChatGPT API Key Load
-<<<<<<< Updated upstream
 os.environ["OPENAI_API_KEY"] = "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"
-=======
-os.environ["OPENAI_API_KEY"] = "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"
->>>>>>> Stashed changes
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 """---
@@ -40,12 +36,12 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 ---
 """
 # Google Cloud 인증 키 파일 경로 (서비스 계정 키)
-credentials_path = "C:\\Users\\ewqds\\Documents\\GitHub\\-Ai\\ai-i-401313-176ecd5ad2cf.json"
+credentials_path = "C:\\Users\\win\\Documents\\GitHub\\-Ai\\ai-i-401313-176ecd5ad2cf.json"
 
 def job():  
     global last_checked_time
     # 모니터링할 경로를 지정합니다.
-    path = "C:\\Users\\ewqds\\Documents\\GOMRecorder"
+    path = "C:\\Users\\win\\Documents\\GOMRecorder"
     
     # 현재 시간과 마지막으로 확인한 시간 사이에 생성된 모든 파일을 찾습니다.
     for file_name in os.listdir(path):
@@ -87,7 +83,7 @@ def job():
                 # 출력 결과
                 print("STT 결과")
                 print(Message_text)
-                with open("C:\\Users\\ewqds\\Documents\\GitHub\\-Ai\\example.txt", "w") as file:
+                with open("C:\\Users\\win\\Documents\\GitHub\\-Ai\\example.txt", "w") as file:
                     file.write(Message_text)
                 #끝
                 end = time.time()
@@ -170,7 +166,7 @@ def job():
                 #TTS시작
                 start = time.time()
                 # Google Cloud TTS 인증 키 파일 경로 (서비스 계정 키)
-                tts_credentials_path = "C:\\Users\\ewqds\\Documents\\GitHub\\-Ai\\ai-i-401313-176ecd5ad2cf.json"
+                tts_credentials_path = "C:\\Users\\win\\Documents\\GitHub\\-Ai\\ai-i-401313-176ecd5ad2cf.json"
                 # TTS 클라이언트 초기화
                 tts_client = texttospeech.TextToSpeechClient.from_service_account_json(tts_credentials_path)
                 def synthesize_text_to_audio(text, output_filename="C:\\Users\\win\\Documents\\GitHub\\-Ai\\tts_output.mp3"):
@@ -200,7 +196,7 @@ def job():
                 # "년월일시분" 형식으로 포맷팅합니다.
                 formatted_time = current_time.strftime("%Y%m%d%H%M%S")
                 # ChatGPT로부터 얻은 답변을 음성으로 변환
-                synthesize_text_to_audio(answer, output_filename = "C:\\Users\\ewqds\\Documents\\GitHub\\-Ai\\output_audio\\" + formatted_time + ".mp3")
+                synthesize_text_to_audio(answer, output_filename = "C:\\Users\\win\\Documents\\GitHub\\-Ai\\output_audio\\" + formatted_time + ".mp3")
                 #끝
                 end = time.time()
                 print(f"TTS Time: {end-start:.5f}sec")
@@ -220,7 +216,7 @@ def job():
                 # Initialize pygame
                 pygame.init()
                 # Load the MP3 file
-                pygame.mixer.music.load("C:\\Users\\ewqds\\Documents\\GitHub\\-Ai\\output_audio\\" + formatted_time + ".mp3")
+                pygame.mixer.music.load("C:\\Users\\win\\Documents\\GitHub\\-Ai\\output_audio\\" + formatted_time + ".mp3")
                 # Play the music
                 pygame.mixer.music.play()
 
