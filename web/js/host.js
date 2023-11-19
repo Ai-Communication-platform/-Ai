45e1342c4942e13ca5a8bd667d7e63f0d225dabc
김기흥 <ewqdsacxzzaqxswcde@gmail.com>
2023-11-20 01:05:26 +0900

    2023-11-20

--- web/js/host.js
+++ web/js/host.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const lamejs = require('lamejs');
+const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 
 const app = express();
@@ -11,48 +11,24 @@ const upload = multer({ dest: 'uploads/' });
 
 app.post('/upload', upload.single('audioFile'), (req, res) => {
     const filePath = req.file.path;
-    const mp3FilePath = filePath.replace('.wav', '.mp3');
+    const outputFilePath = req.file.path + '.mp3';
 
-    fs.readFile(filePath, (err, buffer) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error reading file');
-        }
-
-        // WAV 파일을 MP3로 변환
-        const wav = new lamejs.WavHeader();
-        wav.readHeader(new DataView(new Uint8Array(buffer).buffer));
-        const samples = new Int16Array(buffer, wav.dataOffset, wav.dataLen / 2);
-        const mp3Encoder = new lamejs.Mp3Encoder(wav.channels, wav.sampleRate, 128);
-        const mp3Data = [];
-
-        const sampleBlockSize = 1152; // LAME의 기본 샘플 블록 크기
-        for (let i = 0; i < samples.length; i += sampleBlockSize) {
-            const sampleChunk = samples.subarray(i, i + sampleBlockSize);
-            const mp3buf = mp3Encoder.encodeBuffer(sampleChunk);
-            if (mp3buf.length > 0) {
-                mp3Data.push(new Int8Array(mp3buf));
-            }
-        }
-
-        // 마지막 프레임 처리
-        const finalMp3Buf = mp3Encoder.flush();
-        if (finalMp3Buf.length > 0) {
-            mp3Data.push(new Int8Array(finalMp3Buf));
-        }
-
-        // MP3 파일 생성
-        const mp3Buffer = Buffer.concat(mp3Data);
-        fs.writeFile(mp3FilePath, mp3Buffer, (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Error writing MP3 file');
-            }
-            fs.unlinkSync(filePath); // 원본 파일 삭제
-            res.send('Audio converted to MP3');
-        });
-    });
+    ffmpeg(filePath)
+        .toFormat('mp3')
+        .on('end', function() {
+            console.log('File has been converted successfully');
+            fs.unlink(filePath, (err) => { // 원본 파일 삭제
+                if (err) console.error('Error deleting original file:', err);
+            });
+            res.send({ message: 'Audio converted to MP3', file: outputFilePath });
+        })
+        .on('error', function(err) {
+            console.error('Error converting file:', err);
+            res.status(500).send('Error converting audio');
+        })
+        .save(outputFilePath);
 });
+
 
 const port = 3000;
 app.listen(port, () => {
